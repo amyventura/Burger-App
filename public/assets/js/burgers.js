429adffacd1876bf758dcb7359b5bc28dd9b3282
Amy Ventura <amyvntr@yahoo.com>
2020-11-04 17:04:52 -0500

    added handelbars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+$(document).ready(function() {
   $(".devour").on("click", function(event) {
     event.preventDefault();
     
